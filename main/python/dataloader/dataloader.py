13879417e32b4bf26d1f4ff25f010ee4c3a40b3d
ShangxuanWu <wushx6@gmail.com>
2018-01-25 02:00:10 +0800

    added utils folder

--- main/python/dataloader/dataloader.py
+++ main/python/dataloader/dataloader.py
@@ -1,17 +1,19 @@
 # Copyright Shangxuan Wu @ Myriad of Things
 # Last Modified: 24 Jan 2018
 
-from import DataLoaderConfig
+from main.python.dataloader import DataLoaderConfig
+from main.python.utils import fileUtils
 
 def checkFolderValid(fd):
     # assert 3 files exists in the given folder
     for fn in DataLoaderConfig.fns:
-        assert 
+        if not fileUtils.isFileExists(os.path.join(fd, fn)):
+            return False
     return True
 
 def loadData(fd):
     assert checkFolderValid(fd)
-    
+
     return
 
 
---
+++ main/python/utils/fileUtils.py
@@ -0,0 +1,7 @@
+# CopyRight Shangxuan Wu @ Myriad of Things
+# Created in Jan 2018
+
+import os, pdb
+
+def isFileExists(file_path):
+    return os.path.isfile(file_path)
