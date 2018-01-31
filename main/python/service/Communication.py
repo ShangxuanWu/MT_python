f3ebea8176356fd2db75cc95e2a88b2a436fc315
ShangxuanWu <wushx6@gmail.com>
2018-01-31 02:29:24 -0500

    aaaaa

--- main/python/service/Communication.py
+++ main/python/service/Communication.py
@@ -1,16 +1,22 @@
 # CopyRight Shangxuan Wu @ Myriad of Things
 # File Created in Jan 2018
 
-def sendModel(path):
-    return
+class Communication:
+    # receive raw data from server java code
+    def receiveRawData(self, path, time_string):
+        return
 
-def receiveModel():
-    return
+    # after finish training, send the model back to the server
+    def sendModel(self, path, time_string):
+        return
 
-def getTrainingStatus():
-    return
+    # this function is not implemented yet
+    def receiveModel(self):
+        return
 
-
+    # this function is not implemented yet because we are using pull as UI updating scheme
+    def sendTrainingStatus():
+        return
 
 if __name__ == "__main__":
     pass
--- main/python/testing/BaseTest.py
+++ main/python/testing/BaseTest.py
@@ -1,5 +1,8 @@
 class BaseTest():
-    def __init__():
+    def __init__(self, path):
         return
 
     def 
+
+    def forward(self):
+        return result
